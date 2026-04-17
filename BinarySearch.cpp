ca1eadfd670950abafc31d46898f19b9afb908b3
scrollshadow <zaldhypramana@gmail.com>
2026-04-17 14:42:09 +0700

    1st

---
+++ BinarySearch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> nPanjang;
+    }
+}

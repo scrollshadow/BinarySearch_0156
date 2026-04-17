c23c865a909447c03c3c93d733963b07a809821f
scrollshadow <zaldhypramana@gmail.com>
2026-04-17 15:33:50 +0700

    10

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -102,7 +102,16 @@ void binarySearch()
             {
                 low = mid + 1;
             }
-        } while (condition);
+
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
         
     } while (condition);
     
